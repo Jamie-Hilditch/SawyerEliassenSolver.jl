331ce3736e599e00d2ab88168d97a54101f182de
Jamie Hilditch <hilditch@stanford.edu>
2024-10-04 21:09:13 -0700

    actually clean up h5 files

--- docs/make.jl
+++ docs/make.jl
@@ -89,6 +89,6 @@ makedocs(;
 
 # clean up h5 files
 @info "Cleaning up temporary .h5 created by examples..."
-rm(joinpath(OUTPUT_DIR, "*.h5"))
+readdir(OUTPUT_DIR, join=true) |> filter(x -> endswith(x, ".h5")) .|> rm
 
 deploydocs(; repo="github.com/Jamie-Hilditch/SawyerEliassenSolver.git")
