10bb2f2027491428782a8dc227c76bd07925b651
Jamie Hilditch <hilditch@stanford.edu>
2024-10-07 11:46:51 -0700

    correct form for version number literals

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 # this is a hack to avoid a bug in JET.jl when running CI on Julia 1.8
-if get(ENV, "CI", nothing) == "true" && VERSION == v"1.8"
+if get(ENV, "CI", nothing) == "true" && (v"1.8" <= VERSION < v"1.9-")
     using Pkg
     Pkg.add("JuliaInterpreter@0.9.35")
 end
