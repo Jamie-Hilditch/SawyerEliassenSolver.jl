62ce5c0f7e71f0d2f9b952fcbf45b5c414f5a20f
Jamie Hilditch <hilditch@stanford.edu>
2025-02-19 11:26:02 -0800

    rename variables in integrate_periodic_gradients to be more general

--- src/Output/integrate_gradients.jl
+++ src/Output/integrate_gradients.jl
@@ -3,31 +3,31 @@
 Integrate periodic gradients.
 """
 function integrate_periodic_gradients(
-    Bx::XZVariable, Bz::XZVariable; out::XZVariable=XZVariable(Bx), FZ_working_space::FZVariable=FZVariable(Bx)
+    x_derivative::XZVariable, z_derivative::XZVariable; out::XZVariable=XZVariable(x_derivative), FZ_working_space::FZVariable=FZVariable(x_derivative)
 )
-    @boundscheck consistent_domains(Bx, Bz, out)
-    domain = get_domain(Bx)
-    Bx_FZ = FZ_working_space
+    @boundscheck consistent_domains(x_derivative, z_derivative, out)
+    domain = get_domain(x_derivative)
+    x_derivative_FZ = FZ_working_space
 
     # first get horizontal buoyancy gradient in FZ space
-    horizontal_transform!(Bx_FZ,Bx)
+    horizontal_transform!(x_derivative_FZ,x_derivative)
 
     # save the mean component and integrate the rest (note the integration sets the mean to 0)
-    Bx_mean = Bx_FZ[1, :] ./ size(Bx, 1)
-    ∫dx!(Bx_FZ)
+    x_derivative_mean = x_derivative_FZ[1, :] ./ size(x_derivative, 1)
+    ∫dx!(x_derivative_FZ)
 
     # transform back to physical space and add on linear part
-    horizontal_transform!(out, Bx_FZ)
-    out .+= xgridpoints(domain) * Bx_mean'
+    horizontal_transform!(out, x_derivative_FZ)
+    out .+= xgridpoints(domain) * x_derivative_mean'
 
     # Now we need to add on the mean z dependence
-    Bz_mean = mean(Bz; dims=1)
+    z_derivative_mean = mean(z_derivative; dims=1)
 
-    # integrate Bz_mean
+    # integrate z_derivative_mean
     # use trapezoid rule but don't subtract half the first value
     # this is equivalent to setting the horizontal mean component to zero on the bottom boundary
-    B_mean = (cumsum(Bz_mean; dims=2) .- Bz_mean ./ 2) .* zstepsize(domain)
-    out .+= B_mean
+    mean = (cumsum(z_derivative_mean; dims=2) .- z_derivative_mean ./ 2) .* zstepsize(domain)
+    out .+= mean
 
     return out
 end
