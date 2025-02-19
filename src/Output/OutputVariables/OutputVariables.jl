c5785c0873b1756b9a3ba1960ca47ce63d571269
Jamie Hilditch <hilditch@stanford.edu>
2025-02-18 17:48:24 -0800

    use capital Psi for all streamfunction variables

--- src/Output/OutputVariables/OutputVariables.jl
+++ src/Output/OutputVariables/OutputVariables.jl
@@ -10,7 +10,7 @@ using SawyerEliassenSolver.Output
 using SawyerEliassenSolver.Variables: Tᴴ!, Tˢ!, Tᶜ!
 
 export u,
-    ∂u∂x, ∂²u∂x², ∂u∂z, ∂²u∂z², ∂²u∂x∂z, v, w, ∂w∂x, ∂²w∂x², ∂w∂z, ∂²w∂z², ∂²w∂x∂z, b, ψ, ζ
+    ∂u∂x, ∂²u∂x², ∂u∂z, ∂²u∂z², ∂²u∂x∂z, v, w, ∂w∂x, ∂²w∂x², ∂w∂z, ∂²w∂z², ∂²w∂x∂z, b, Ψ, ζ
 
 include("u_and_derivatives.jl")
 include("v_and_derivatives.jl")
--- src/Output/OutputVariables/psi_vorticity.jl
+++ src/Output/OutputVariables/psi_vorticity.jl
@@ -1,15 +1,15 @@
-function compute_ψ!(problem::Problem{T}, ψ::XZVariable{T}) where {T}
+function compute_Ψ!(problem::Problem{T}, Ψ::XZVariable{T}) where {T}
     @boundscheck consistent_domains(ψ, problem) ||
         throw(ArgumentError("`ψ` and `problem` must have the same domain."))
     # use the scratch variables for storage
-    Ψ_FS, ψ_XS = problem.scratch.FS_tmp, problem.scratch.XS_tmp
+    Ψ_FS, Ψ_XS = problem.scratch.FS_tmp, problem.scratch.XS_tmp
 
     # first compute the streamfunction Ψ in FS space from the vorticity ζ
     ∇⁻²!(Ψ_FS, problem.state.ζ)
 
     # transform to XZ
-    Tᴴ!(ψ_XS, ψ_FS)
-    Tˢ!(Ψ, ψ_XS)
+    Tᴴ!(Ψ_XS, Ψ_FS)
+    Tˢ!(Ψ, Ψ_XS)
 
     return nothing
 end
@@ -18,7 +18,7 @@ function compute_ζ!(problem::Problem{T}, ζ::XZVariable{T}) where {T}
     @boundscheck consistent_domains(ζ, problem) ||
         throw(ArgumentError("`ζ` and `problem` must have the same domain."))
     # use the scratch variables for storage
-    ζ_FS, ζ_XS = problem.scratch.FS_tmp, problem.scratch.XS_tmp
+    ζ_XS = problem.scratch.XS_tmp
 
     # transform to XZ
     Tᴴ!(ζ_XS, problem.state.ζ)
@@ -28,11 +28,17 @@ function compute_ζ!(problem::Problem{T}, ζ::XZVariable{T}) where {T}
 end
 
 # define unsafe versions of the functions with no bounds checking
-@inline _compute_ψ!(problem, ψ) = @inbounds compute_ψ!(problem, ψ)
+@inline _compute_Ψ!(problem, Ψ) = @inbounds compute_Ψ!(problem, Ψ)
 @inline _compute_ζ!(problem, ζ) = @inbounds compute_ζ!(problem, ζ)
 
 # define the output variables
-"""$(TYPEDSIGNATURES)"""
-ψ(problem::Problem) = OutputVariable(problem, _compute_ψ!, (:x, :z), problem.scratch.XZ_tmp)
-"""$(TYPEDSIGNATURES)"""
+"""$(TYPEDSIGNATURES)
+
+The streamfunction Ψ.
+"""
+Ψ(problem::Problem) = OutputVariable(problem, _compute_ψ!, (:x, :z), problem.scratch.XZ_tmp)
+"""$(TYPEDSIGNATURES)
+
+The vorticity ζ.
+"""
 ζ(problem::Problem) = OutputVariable(problem, _compute_ζ!, (:x, :z), problem.scratch.XZ_tmp)
