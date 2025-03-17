5c65637387cea8be1f0d3bfba7f157a266f851c9
Jamie Hilditch <hilditch@stanford.edu>
2025-03-16 19:02:22 -0700

    spectral forcing with nothing

--- src/Forcing/spectral_forcing.jl
+++ src/Forcing/spectral_forcing.jl
@@ -4,6 +4,8 @@
 Apply pointwise forcing to the Sawyer-Eliassen equation in spectral space.
 `func` is a function that computes the forcing at given wavenumbers and time.
 `func` should have signature `func(kx::T,kz::T,t::T,params::P)::complex{T} where {T<:Union{Float32,Float64}}`.
+unless `params` is `nothing`, in which case `func` should have signature
+`func(kx::T,kz::T,t::T)::T where {T<:Union{Float32,Float64}}`
 
 # Fields
 $(TYPEDFIELDS)
@@ -17,9 +19,21 @@ end
 """
     $(TYPEDEF)
 
+Pointwise spectral forcing with no parameters.
+`func` should have signature `func(kx::T,kz::T,t::T)::T where {T<:Union{Float32,Float64}}`.
+"""
+function PointwiseSpectralForcing(domain::Domain{T}, func::Function) where {T}
+    return PointwiseSpectralForcing{T,Nothing}(domain, func, nothing)
+end
+
+"""
+    $(TYPEDEF)
+
 Apply global forcing to the Sawyer-Eliassen equation in spectral space.
 `func` is a function that updates the entire forcing variable in-place at time `t`.
-`func` should have signature `func(F::FSVariable{T},t::T,params::P)::Nothing where {T<:Union{Float32,Float64}}`.
+`func` should have signature `func(F::FSVariable{T},t::T,params::P)::Nothing where {T<:Union{Float32,Float64}}`
+unless `params` is `nothing`, in which case `func` should have signature
+`func(F::FSVariable{T},t::T)::T where {T<:Union{Float32,Float64}}`
 Note that the `FSVariable{T}` has entries of type `Complex{T}`.
 
 # Fields
@@ -31,33 +45,69 @@ struct GlobalSpectralForcing{T,P} <: AbstractForcing{T}
     params::P
 end
 
+"""
+    $(TYPEDEF)
+
+Global spectral forcing with no parameters.
+`func` should have signature `func(F::FSVariable{T},t::T)::T where {T<:Union{Float32,Float64}}`.
+"""
+function GlobalSpectralForcing(domain::Domain{T}, func::Function) where {T}
+    return GlobalSpectralForcing{T,Nothing}(domain, func, nothing)
+end
+
 """Union of types representing forcing in spectral (FS) space."""
-SpectralForcing{T} =
-    Union{PointwiseSpectralForcing{T,P},GlobalSpectralForcing{T,Q}} where {T,P,Q}
+SpectralForcing{T} = Union{PointwiseSpectralForcing{T,P},GlobalSpectralForcing{T,Q}} where {T,P,Q}
 
 ####################
 # Evaluate forcing #
 ####################
 
 @inline function evaluate_ζ_forcing!(
+    F::SpectralForcing{T},
+    out::FSVariable{T},
+    t::T,
+    XS::XSVariable{T},
+    XZ::XZVariable{T}
+) where {T}
+    @boundscheck consistent_domains(F, out) ||
+    throw(ArgumentError("`F` and `out` must have the same domain."))
+    _evaluate_ζ_forcing!(F, out, t, XS, XZ)
+end
+
+@inline function _evaluate_ζ_forcing!(
     F::PointwiseSpectralForcing{T,P},
     out::FSVariable{T},
     t::T,
     ::XSVariable{T},
     ::XZVariable{T},
 ) where {T,P}
-    @boundscheck consistent_domains(F, out) ||
-        throw(ArgumentError("`F` and `out` must have the same domain."))
     kx, kz = wavenumbers_full(F)
-    @inbounds out .= F.func.(kx, kz, tuple(t), tuple(F.params))
+    @inbounds out .= F.func.(kx, kz, t, tuple(F.params))
     return nothing
 end
 
-@inline function evaluate_ζ_forcing!(
+@inline function _evaluate_ζ_forcing!(
+    F::PointwiseSpectralForcing{T,Nothing},
+    out::FSVariable{T},
+    t::T,
+    ::XSVariable{T},
+    ::XZVariable{T},
+) where {T}
+    kx, kz = wavenumbers_full(F)
+    @inbounds out .= F.func.(kx, kz, t)
+    return nothing
+end
+
+@inline function _evaluate_ζ_forcing!(
     F::GlobalSpectralForcing{T,P}, out::FSVariable{T}, t::T, ::XSVariable, ::XZVariable
 ) where {T,P}
-    @boundscheck consistent_domains(F, out) ||
-        throw(ArgumentError("`F` and `out` must have the same domain."))
     F.func(out, t, F.params)
+    return nothing
+end
+
+@inline function _evaluate_ζ_forcing!(
+    F::GlobalSpectralForcing{T,Nothing}, out::FSVariable{T}, t::T, ::XSVariable, ::XZVariable
+) where {T}
+    F.func(out, t)
     return nothing
 end
